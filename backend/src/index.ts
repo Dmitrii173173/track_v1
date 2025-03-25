c67688b565ac19a78fb5a4af1d06fceda1a8bdba
dmitrii <173dmitt173@gmail.com>
2025-03-25 18:50:06 +0800

    commit 123

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ dotenv.config()
 
 const app = express()
 const prisma = new PrismaClient()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8080
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
